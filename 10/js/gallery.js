2cb4d841dcc9e37fbf628a23436275bcbdf14b43
Keks <keks@htmlacademy.ru>
2026-09-17 00:34:23 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/gallery.js
@@ -0,0 +1,32 @@
+import { galleryParameters } from './parameters.js';
+import { createGalleryContent } from './gallery-content.js';
+import { showBigPhoto } from './show-big-photo.js';
+
+const galleryContent = createGalleryContent();
+const galleryContainer = document.querySelector(galleryParameters.GALLERY_CONTAINER);
+const galleryTemplate = document.querySelector(galleryParameters.GALLERY_TEMPLATE).content;
+const galleryFragment = document.createDocumentFragment();
+
+const createGallery = () => {
+  galleryContent.forEach((galleryElement) => {
+    const galleryContainerElement = galleryTemplate.cloneNode(true);
+    const link = galleryContainerElement.querySelector(galleryParameters.GALLERY_TEMPLATE_LINK);
+    link.href = galleryElement.url;
+    galleryContainerElement.querySelector(galleryParameters.GALLERY_TEMPLATE_IMAGE).
+      src = galleryElement.url;
+    galleryContainerElement.querySelector(galleryParameters.GALLERY_TEMPLATE_IMAGE).
+      alt = galleryElement.description;
+    galleryContainerElement.querySelector(galleryParameters.GALLERY_TEMPLATE_COMMENTS).
+      textContent = galleryElement.comments.length;
+    galleryContainerElement.querySelector(galleryParameters.GALLERY_TEMPLATE_LIKES).
+      textContent = galleryElement.likes;
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      showBigPhoto(galleryElement);
+    });
+    galleryFragment.appendChild(galleryContainerElement);
+  });
+  galleryContainer.appendChild(galleryFragment);
+};
+
+export { createGallery };
---
+++ 10/js/main.js
@@ -0,0 +1,5 @@
+import { createGallery } from './gallery.js';
+import { uploadForm } from './upload-form.js';
+
+createGallery();
+uploadForm();
